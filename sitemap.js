c4849cb4f39fac5dc2b88d2c3c5a0222283941d7
Axel Hunter <hunter.a@cambridgeassessment.org.uk>
2016-10-19 18:34:40 +0100

    added cron job for sitemap

--- sitemap.js
+++ sitemap.js
@@ -2,17 +2,22 @@
 const SitemapGenerator = require('sitemap-generator');
 const fs = require('fs');
 const path = require('path');
-const settings = require('../settings/settings')
-  .production;
+const CronJob = require('cron')
+  .CronJob;
 
 const generator = new SitemapGenerator('http://www.bluebulldog.co.uk');
 
 generator.on('done', (sitemap) => {
-  fs.writeFile(path.join(settings.rootPath, '/sitemap.xml'), sitemap, (err) => {
+  fs.writeFile(path.join('dist/sitemap.xml'), sitemap, (err) => {
     if (!err) {
       return console.log('The sitemap was saved!');
     }
     return console.log(err);
   });
 });
+
 generator.start();
+
+new CronJob('* * * 1 * *', () => {
+  generator.start();
+}, null, true, 'America/Los_Angeles');
